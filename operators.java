68ad9a2ed9ffc0794dc990828fbe17703fc51c61
RITIK KUMAR <ritikkumar12bicbly@gmail.com>
2025-04-13 11:44:23 +0530

    I created the operators.java file

---
+++ operators.java
@@ -0,0 +1,13 @@
+class operators
+	{
+		public static void main(String [] args)
+	{
+        int a = 50;
+        int b = 50;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+        System.out.println(a%b);
+	}
+	}
